ebcf0504841e8ea75155942e579c9ba0d2fd69a3
kevin-robben <84887999+kevin-robben@users.noreply.github.com>
2021-07-13 17:10:55 -0500

    Update M_3rd_order_kubo.m

--- Manuscript Examples/Sim Over param/Lineshape Functions/M_3rd_order_kubo.m
+++ Manuscript Examples/Sim Over param/Lineshape Functions/M_3rd_order_kubo.m
@@ -1,8 +1,8 @@
 %% 3rd order FID: Kubo + pure dephasing model
 function FID = M_3rd_order_kubo(x,p)
 %% generate FID
-		  %    non-rephasing              rephasing
-	FID = fft(Rnr(x,p),2*x.N3,2) + conj(fft(Rr(x,p),2*x.N3,2)) ;
+		           %    non-rephasing                                     rephasing
+	FID = exp(1i*p.phi.val/180*pi)*fft(Rnr(x,p),2*x.N3,2) + exp(-1i*p.phi.val/180*pi)*conj(fft(Rr(x,p),2*x.N3,2)) ;
 	FID = FID(:,1:x.N3,:); % discard negative time half
 	if strcmp(x.num_type,'real')
 		FID = real(FID);
